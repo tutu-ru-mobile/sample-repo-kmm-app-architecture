595b4ebd3a58d1ad8fa37ea14210b38045102177
avdim <avdim@mail.ru>
2020-08-17 13:47:49 +0300

    simplify iOS

--- solution-tabs-impl/src/iOSMain/swift/src/SolutionTabsIOSImpl.swift
+++ solution-tabs-impl/src/iOSMain/swift/src/SolutionTabsIOSImpl.swift
@@ -4,29 +4,42 @@ import app_di
 import solution_tabs_api_swift
 import solution_tab_search_api_swift
 
-public struct SolutionTabsIOSImpl<S1: SolutionTabSearchIosApi>: SolutionTabsIOSApi {
+public struct SolutionTabsIOSImpl<TSolutionTabSearchIosApi: SolutionTabSearchIosApi>: SolutionTabsIOSApi {
 
     var common: Solution_tabs_implSolutionTabsImpl
-    var searchTabIos: S1
+    var searchTabIos: TSolutionTabSearchIosApi
 
     public init(
             common: Solution_tabs_implSolutionTabsImpl,
-            searchTabIos: S1
+            searchTabIos: TSolutionTabSearchIosApi
     ) {
         self.common = common
         self.searchTabIos = searchTabIos
     }
 
     public func renderBottomNavigation() -> some View {
-        HStack {
-            Text("todo renderBottomNavigation")
+        HStack() {
+            Button(action: {
+                self.common.store.send(action: Solution_tabs_implSolutionTabsImpl.ActionSelectMain())
+            }) {
+                Text("Main")
+            }.padding()
+            Button(action: {
+                self.common.store.send(action: Solution_tabs_implSolutionTabsImpl.ActionSelectOrders())
+            }) {
+                Text("Orders")
+            }.padding()
+            Button(action: {
+                self.common.store.send(action: Solution_tabs_implSolutionTabsImpl.ActionSelectSettings())
+            }) {
+                Text("Settings")
+            }.padding()
         }
     }
 
     public func renderScaffold() -> some View {
-        let state: Solution_tabs_implSolutionTabsImpl.State =
-                common.store.state as! Solution_tabs_implSolutionTabsImpl.State
-        let screen: Solution_tabs_implSolutionTabsImpl.Screen = state.screen
+        let state = common.store.state as! Solution_tabs_implSolutionTabsImpl.State
+        let screen = state.screen
 
         return VStack(alignment: .center) {
             if (screen is Solution_tabs_implSolutionTabsImpl.ScreenMain) {
@@ -39,23 +52,7 @@ public struct SolutionTabsIOSImpl<S1: SolutionTabSearchIosApi>: SolutionTabsIOSA
                 Text("Settings")
             }
             Spacer()
-            HStack() {
-                Button(action: {
-                    self.common.store.send(action: Solution_tabs_implSolutionTabsImpl.ActionSelectMain())
-                }) {
-                    Text("Main")
-                }.padding()
-                Button(action: {
-                    self.common.store.send(action: Solution_tabs_implSolutionTabsImpl.ActionSelectOrders())
-                }) {
-                    Text("Orders")
-                }.padding()
-                Button(action: {
-                    self.common.store.send(action: Solution_tabs_implSolutionTabsImpl.ActionSelectSettings())
-                }) {
-                    Text("Settings")
-                }.padding()
-            }
+            renderBottomNavigation()
         }.padding()
 
     }
