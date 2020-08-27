a2c6520e71b6695b66f8476f0d86f560f065d407
avdim <avdim@mail.ru>
2020-08-27 22:17:58 +0300

    autoManifest, simplify versions.kt

--- buildSrc/src/main/kotlin/versions.kt
+++ buildSrc/src/main/kotlin/versions.kt
@@ -1,31 +1,20 @@
 import java.text.SimpleDateFormat
 import java.util.*
 
-//const val KOTLIN_VERSION = "1.4-M3"
-//const val COROUTINES_VERSION = "1.3.7-1.4-M3"
 const val KOTLIN_VERSION = "1.4.0"
-//const val KOTLIN_VERSION = "1.4.0-release-324"
-//const val KOTLIN_VERSION = "1.4.20-SNAPSHOT"
-//const val KOTLIN_VERSION = "1.4.255-SNAPSHOT"
-//const val COROUTINES_VERSION = "1.3.8-1.4.0-rc"
-//const val COROUTINES_VERSION = "1.3.8-native-mt-1.4.0-rc"
 //const val COROUTINES_VERSION = "1.3.9"
-//const val COROUTINES_NATIVE_VERSION = "1.3.8-native-mt-1.4.0-rc"//https://bintray.com/kotlin/kotlinx/kotlinx.coroutines
 const val COROUTINES_NATIVE_VERSION = "1.3.9-native-mt"
 const val COROUTINES_VERSION = COROUTINES_NATIVE_VERSION
-//const val KTOR_VERSION = "1.3.2-1.4.0-rc"
 const val KTOR_VERSION = "1.4.0"
 
 //https://developer.android.com/jetpack/androidx/releases/compose
-//const val ANDROID_COMPOSE_VERSION = "0.1.0-dev15"
-//const val ANDROID_COMPOSE_VERSION = "0.1.0-dev17"
 const val ANDROID_COMPOSE_VERSION = "1.0.0-alpha01"
 //const val ANDROID_KOTLIN_COMPILER_VERSION = "1.4.0-dev-withExperimentalGoogleExtensions-20200720"
-const val ANDROID_KOTLIN_COMPILER_VERSION = "1.4.0"
+//const val ANDROID_KOTLIN_COMPILER_VERSION = "1.4.0"
+val ANDROID_KOTLIN_COMPILER_VERSION = null
 
 const val ANDROID_COMPILE_SDK = 29
 const val ANDROID_BUILD_TOOLS_VERSION = "29.0.2"
 const val ANDROID_MIN_SDK = 21
 const val ANDROID_TARGET_SDK = 21
 const val ANDROID_APP_COMPAT = "1.1.0"
-const val AUTO_MANIFEST_VERSION = "1.0.2"
