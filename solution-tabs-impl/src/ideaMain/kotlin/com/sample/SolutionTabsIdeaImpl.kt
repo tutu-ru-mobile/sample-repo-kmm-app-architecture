fe68868adcb8ef7816317b6b80e56c92b05044c2
avdim <avdim@mail.ru>
2020-09-26 21:46:25 +0300

    SolutionTabsIdeaImpl

--- solution-tabs-impl/src/ideaMain/kotlin/com/sample/SolutionTabsIdeaImpl.kt
+++ solution-tabs-impl/src/ideaMain/kotlin/com/sample/SolutionTabsIdeaImpl.kt
@@ -1,7 +1,4 @@
-
 package com.sample
-
-import kotlin.random.Random
 
 class SolutionTabsIdeaImpl(
     val commonImpl: SolutionTabsImpl
@@ -12,7 +9,30 @@ class SolutionTabsIdeaImpl(
 
     override fun renderScaffold(): IdeaPanel {
         return panelView {
+            val state = commonImpl.store.state
+            when (state.screen) {
+                is SolutionTabsImpl.Screen.Main -> {
+                    label("SolutionTabsImpl.Screen.Main")
+                }
+                is SolutionTabsImpl.Screen.Orders -> {
+                    label("SolutionTabsImpl.Screen.Orders")
+                }
+                is SolutionTabsImpl.Screen.Settings -> {
+                    label("SolutionTabsImpl.Screen.Settings")
+                }
+            }
             label("todo")
+            row {
+                button("Поиск") {
+                    commonImpl.store.send(SolutionTabsImpl.Action.SelectMain)
+                }
+                button("Мои Билеты") {
+                    commonImpl.store.send(SolutionTabsImpl.Action.SelectOrders)
+                }
+                button("Настройки") {
+                    commonImpl.store.send(SolutionTabsImpl.Action.SelectSettings)
+                }
+            }
         }
     }
 
